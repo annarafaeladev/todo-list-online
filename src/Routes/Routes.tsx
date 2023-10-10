3c7acce3b22949dddababa9958aca3462392b958
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-10-10 17:01:59 -0300

    feat: add new page /home

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { Login } from "../pages/Auth/Login/Login";
 import { Singup } from "../pages/Auth/Singup/Singup";
+import { Home } from "../pages/Home/Home";
 
 export const Routes = () => {
   const router = createBrowserRouter([
@@ -12,6 +13,10 @@ export const Routes = () => {
     {
       path: "/auth",
       element: <Singup />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
     }
   ], {
     basename: '/todo-list-online'
